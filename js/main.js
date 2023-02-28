3afa12d3c594f7b7b5cab5f328fc1fb7f56374d6
danielebd <danielebalebalestro@gmail.com>
2023-02-28 15:08:04 +0100

    mode: aggiungi altri elemnti alla lista

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 const list = ['pane', 'pasta', 'carote', 'p.cotto', 'germogli di soia', 'avocado']
 const ul = document.querySelector('.list');
-console.log(list);
+
 let itemContent = '';
 let i = 0;
 while(i < list.length){
@@ -13,3 +13,12 @@ while(i < list.length){
     ul.append(itemContent);
     i++;
 }
+
+const input = document.getElementById('input');
+const button = document.querySelector('#button');
+
+button.addEventListener('click', function(){
+    itemContent = document.createElement('li');
+    itemContent.append(input.value);
+    ul.append(itemContent);
+})
