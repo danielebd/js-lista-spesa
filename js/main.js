0c863aa0c17741f41345812581910e333d09503f
danielebd <danielebalebalestro@gmail.com>
2023-02-28 14:48:57 +0100

    lista

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const list = ['pane', 'pasta', 'carote', 'p.cotto', 'germogli di soia', 'avocado']
+const ul = document.querySelector('.list');
+console.log(list);
+let itemContent = '';
+let i = 0;
+while(i < list.length){
+    
+    itemContent = document.createElement('li');
+    console.log(itemContent);
+    itemContent.append(list[i]);
+    ul.append(itemContent);
+    i++;
+}
